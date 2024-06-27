f7994f608c290dcba3005ae89c965734952caf45
Vladimir Makarenko <macmacins@inbox.ru>
2024-06-27 09:41:47 +0200

    fix: add shopId to the startConfirmation meth

--- src/types/ConfirmationParameters.ts
+++ src/types/ConfirmationParameters.ts
@@ -1,6 +1,7 @@
 import type { PaymentMethodType } from './PaymentMethodType';
 
 export type ConfirmationParameters = {
+  shopId: string;
   confirmationUrl: string;
   paymentMethodType: PaymentMethodType;
   clientApplicationKey: string;
